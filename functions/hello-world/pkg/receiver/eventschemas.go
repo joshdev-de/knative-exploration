aa1bae84650ac49dd8628a1af080e6727466091f
joshdev-de <joshua.steinmann@ru.nl>
2023-01-09 12:13:08 +0100

    try fix

--- functions/hello-world/pkg/receiver/eventschemas.go
+++ functions/hello-world/pkg/receiver/eventschemas.go
@@ -3,11 +3,11 @@ package receiver
 // HelloWorld defines the Data of CloudEvent with type=dev.knative.samples.helloworld
 type HelloWorld struct {
 	// Msg holds the message from the event
-	Msg string `json:"msg,omitempty,string"`
+	Msg string `json:"msg,omitempty"`
 }
 
 // HiFromKnative defines the Data of CloudEvent with type=dev.knative.samples.hifromknative
 type HiFromKnative struct {
 	// Msg holds the message from the event
-	Msg string `json:"msg,omitempty,string"`
+	Msg string `json:"msg,omitempty"`
 }
